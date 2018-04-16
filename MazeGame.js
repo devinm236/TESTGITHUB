40a6708f0a8e17cd7ba50a84ace0f40999f0fa0c
devinm236 <35576402+devinm236@users.noreply.github.com>
2018-04-16 16:43:59 -0500

    Add files via upload

---
+++ MazeGame.js
@@ -0,0 +1,16 @@
+function Maze_Game_1 ()
+{
+  document.getElementById("Maze_Game_1").src = "Maze_Game_Solution_1.jpg";
+}
+function Maze_Game_Solution_1()
+{
+  document.getElementById("Maze_Game_Solution_1").src= "Maze_Game_Solution_1";
+}
+function New_MazeGame ()
+{
+  var Mazes = [
+    "Maze_Game_3.jpg",
+    "Maze_Game_4.jpg",
+  ]
+  var Maze = Mazes[Math.floor(Math.random()*Mazes.length)];
+}
