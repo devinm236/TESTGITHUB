f355855ed87026d167ee02a4ae82634f2eddf1e5
devinm236 <35576402+devinm236@users.noreply.github.com>
2018-04-23 11:27:59 -0500

    Update UserName.js

--- UserName.js
+++ UserName.js
@@ -1,4 +1,5 @@
+//Modified code from W3Schools <!-- This is modified from W3Schools.com https://www.w3schools.com/js/tryit.asp?filename=tryjs_form_submit -->
 function UserName () {
-  var playerName = document.getElementById("playerName").value; 
+  var playerName = document.getElementById("playerName").value;
   document.getElementById("userName").innerHTML = "You submitted your user name " + playerName;
 }
